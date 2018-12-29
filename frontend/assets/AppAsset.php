c6b292e2bd1064899220bb41fd7e3520c58555fd
nostromo76 <43537037+nostromo76@users.noreply.github.com>
2018-12-29 19:04:08 +0100

    Add files via upload

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,6 +12,8 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/main.css',
+        'css/util.css',
         'css/site.css',
     ];
     public $js = [
