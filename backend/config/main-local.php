b32d5dbf18b415d5a7b929490d2e3550cfaf1bd0
ikimj420 <ikimj420@gmail.com>
2019-01-08 23:00:10 +0100

    Pjax i DropDown

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'mJxywtVDDKwIOFy-8BOqJa2um4KS0zfb',
+            'cookieValidationKey' => '0qa6o3vjyXrxXdN8DiwtVNZP8NgfJHll',
         ],
     ],
 ];
--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=dnevnik',
+            'dsn' => 'mysql:host=localhost;dbname=ednevnik',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
