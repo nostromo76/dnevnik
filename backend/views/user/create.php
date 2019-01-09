dea0b1e98d992679b82d7657734903aae9e7059d
Goran Milicevic <antropolog011@gmail.com>
2019-01-10 00:48:44 +0100

    korisnici

--- backend/views/user/create.php
+++ backend/views/user/create.php
@@ -6,13 +6,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\User */
 
-<<<<<<< HEAD
-$this->title = 'Create User';
-$this->params['breadcrumbs'][] = ['label' => 'User', 'url' => ['index']];
-=======
+//<<<<<<< HEAD
+//$this->title = 'Create User';
+//$this->params['breadcrumbs'][] = ['label' => 'User', 'url' => ['index']];
+//=======
 $this->title = 'Kreiraj korisnika';
 $this->params['breadcrumbs'][] = ['label' => 'Korisnici', 'url' => ['index']];
->>>>>>> 01e56c25ffafb472ef0e21be74cbab8fe84690ed
+//>>>>>>> 01e56c25ffafb472ef0e21be74cbab8fe84690ed
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-create">
