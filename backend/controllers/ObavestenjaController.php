501664c6c61cc7ce21efe41db9d167d0247ffaa2
ikimj420 <ikimj420@gmail.com>
2019-01-10 16:21:06 +0100

    obavestenja roditelj ucenin controler

--- backend/controllers/ObavestenjaController.php
+++ backend/controllers/ObavestenjaController.php
@@ -6,6 +6,7 @@ use Yii;
 use backend\models\Obavestenja;
 use backend\models\ObavestenjaSearch;
 use yii\web\Controller;
+use yii\filters\AccessControl;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
@@ -20,6 +21,16 @@ class ObavestenjaController extends Controller
     public function behaviors()
     {
         return [
+            "access"=> [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['index','update','create','delete','view'],
+                        'allow' => true,
+                        'roles' => ['@']
+                    ]
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
