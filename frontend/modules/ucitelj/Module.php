5f09e7177eb83b6a0c01e72af90096683bd7adc0
milosvukasinac <milosvukasinac@gmail.com>
2019-01-14 11:37:49 +0100

    Module

---
+++ frontend/modules/ucitelj/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\ucitelj;
+
+/**
+ * ucitelj module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\ucitelj\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
