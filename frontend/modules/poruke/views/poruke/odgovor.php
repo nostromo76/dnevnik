7a68d56d2b2bfd8a8c70de077671e612c657c9cb
milosvukasinac <milosvukasinac@gmail.com>
2019-01-20 22:29:49 +0100

    chat

---
+++ frontend/modules/poruke/views/poruke/odgovor.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\modules\poruke\models\Poruke */
+
+$this->title = 'Odgovor';
+$this->params['breadcrumbs'][] = ['label' => 'Nazad', 'url' => ['index']];
+
+?>
+<div class="poruke-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'poruka')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+
